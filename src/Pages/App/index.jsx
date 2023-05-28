737a705d380779ce6bf220b0ae4a522aa01cd9f7
Daniel Felipe Ramírez Caro <dframirez4706@outlook.com>
2023-05-28 13:17:30 -0500

    Initial commit

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,36 @@
+import { useRoutes, BrowserRouter } from "react-router-dom";
+import { ShoppingCartProvider } from "../../context";
+import Home from "../Home";
+import MyAccount from "../MyAccount";
+import MyOrder from "../MyOrder";
+import MyOrders from "../MyOrders";
+import NotFound from "../NotFound";
+import SignIn from "../SignIn";
+import NavBar from "../../Components/NavBar";
+import "./App.css";
+
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "/*", element: <NotFound /> },
+  ]);
+
+  return routes;
+};
+
+const App = () => {
+  return (
+    <ShoppingCartProvider>
+      <BrowserRouter>
+        <AppRoutes />
+        <NavBar />
+      </BrowserRouter>
+    </ShoppingCartProvider>
+  );
+};
+
+export default App;
